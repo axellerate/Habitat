eb76906db165b272f2f5b5105e45d96c1c9685cd
admin <axellerate@hotmail.com>
2015-09-15 01:40:42 -0400

    added more models

--- habitat-api/models.py
+++ habitat-api/models.py
@@ -4,7 +4,7 @@ class Base(ndb.model):
 	'''
 		Base model - inherited by all other models.
 	'''
-	
+
 	created = ndb.DateTimeProperty(auto_now_add = True)
 	updated = ndb.DateTimeProperty(auto_now = True)
 
@@ -26,8 +26,25 @@ class Cars(Base):
 	car_type = ndb.StringProperty(required = True)
 	emissions_per_km = ndb.FloatProperty(required = True)
 
+
+class AlternateTransport(Base):
+	'''
+		Stores alternate transport methods.
+		Public Transit, Biking and Walking
+
+	'''
+
+	transport_type = ndb.StringProperty(required = True)
+	emissions_per_km = ndb.FloatProperty(required = True)
+
+
 class TransportMethods(Base):
+	'''
+		Stores the possible methods of transport.
+		Car, Public Transit, Biking and Walking
+	'''
 	method = ndb.StringProperty(required = True)
+
 
 class Distance(Base):
 	'''
@@ -38,3 +55,33 @@ class Distance(Base):
 	user = ndb.KeyProperty(kind = Users)
 	distance = ndb.FloatProperty(required = True)
 	transport_method = ndb.KeyProperty(kind = TransportMethods)
+
+
+class Electricity(Base):
+	'''
+		Stores electricity use information.
+	'''
+
+	user = ndb.KeyProperty(kind = User)
+	bill = ndb.FloatProperty(required = True)
+	cycle_in_days = ndb.IntegerProperty(required = True)
+
+
+class Gas(Base):
+	'''
+		Stores gas use information.
+	'''
+
+	user = ndb.KeyProperty(kind = User)
+	bill = ndb.FloatProperty(required = True)
+	cycle_in_days = ndb.IntegerProperty(required = True)
+
+
+class Water(Base):
+	'''
+		Stores water user information.
+	'''
+
+	user = ndb.KeyProperty(kind = User)
+	bill = ndb.FloatProperty(required = True)
+	cycle_in_days = ndb.IntegerProperty(required = True)
